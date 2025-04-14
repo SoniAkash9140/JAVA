e0b485c436c8e28c419434a10313fc95d8f9613f
Akash Soni <as6171079@gmail.com>
2025-04-15 00:23:14 +0530

    first commit

---
+++ Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+    public 
+static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    public static void greet() {
+        System.out.println("Greetings from the Demo class!");
+    }
+}
